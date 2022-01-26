2c8fbd3510ba72f114207c946117e10b7e67efa7
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-26 17:42:37 +0100

    Add canvas details

--- data/main.py
+++ data/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+LIGHT_BLUE = "#add8e6"
+
 window = tk.Tk()
 window.config(width=800, height=526, pady=50, padx=50)
 window.title("Flashy")
@@ -12,10 +14,16 @@ wrong_button_img = tk.PhotoImage(file="../images/wrong.png")
 wrong_button = tk.Button(image=wrong_button_img, highlightthickness=0)
 wrong_button.grid(column=0, row=1)
 
-card_front_img = tk.PhotoImage(file="../images/card_front.png")
-card_back_img = tk.PhotoImage(file="../images/card_back.png")
 card = tk.Canvas(width=800, height=500, highlightthickness=0)
-card.create_image(400, 250, image=card_front_img)
+card_img = tk.PhotoImage(file="../images/card_front.png")
+card.create_image(400, 250, image=card_img)
+card.create_text(400, 150, text="French", fill="black", font=("Arial", 40, "italic"))
+card.create_text(400, 263, text="some word", fill="black", font=("Arial", 60, "bold"))
 card.grid(column=0, row=0, columnspan=2)
+
+# card_img = tk.PhotoImage(file="../images/card_back.png")
+# card.create_text(400, 150, text="English", fill="black", font=("Arial", 40, "italic"))
+# card.create_text(400, 263, text="some word", fill="black", font=("Arial", 60, "bold"))
+
 
 window.mainloop()
